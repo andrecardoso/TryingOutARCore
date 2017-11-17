ff0a3ca4904ce5b2ce32183f4e3cb5b07c0bc672
André Luiz Cardoso <andrecardoso@gmail.com>
2017-11-17 18:33:00 -0200

    Adding GLSurfaceView

--- app/src/main/java/andrecardoso/tryingoutarcore/MainActivity.kt
+++ app/src/main/java/andrecardoso/tryingoutarcore/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.opengl.GLSurfaceView
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.StringRes
@@ -13,8 +14,11 @@ import android.view.WindowManager
 import android.widget.Toast
 import com.google.ar.core.Config
 import com.google.ar.core.Session
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GLSurfaceView.Renderer {
 
     private companion object {
         val TAG = "MainActivity"
@@ -29,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setFullScreen()
 
+        setupSurfaceView()
         setupARCore()
     }
 
@@ -47,6 +52,23 @@ class MainActivity : AppCompatActivity() {
             showToast(R.string.camera_permission_needed)
             finish()
         }
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+    }
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+    }
+
+    private fun setupSurfaceView() {
+        surfaceView.preserveEGLContextOnPause = true
+        surfaceView.setEGLContextClientVersion(2)
+        surfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0) // Alpha used for plane blending.
+        surfaceView.setRenderer(this)
+        surfaceView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
     }
 
     private fun setupARCore() {
