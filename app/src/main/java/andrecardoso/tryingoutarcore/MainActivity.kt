9995b920f2497288cf9012395d878c921e35991d
André Luiz Cardoso <andrecardoso@gmail.com>
2017-11-17 18:24:37 -0200

    Creating ARCore session

--- app/src/main/java/andrecardoso/tryingoutarcore/MainActivity.kt
+++ app/src/main/java/andrecardoso/tryingoutarcore/MainActivity.kt
@@ -5,8 +5,11 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.StringRes
 import android.util.Log
 import android.widget.Toast
+import com.google.ar.core.Config
+import com.google.ar.core.Session
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +18,14 @@ class MainActivity : AppCompatActivity() {
         val CAMERA_PERMISSION_CODE = 0
     }
 
+    lateinit var session: Session
+    lateinit var config: Config
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupARCore()
     }
 
     override fun onResume() {
@@ -32,12 +40,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, results: IntArray) {
         if (!hasCameraPermission()) {
-            Toast.makeText(this, getString(R.string.camera_permission_needed), Toast.LENGTH_LONG).show()
+            showToast(R.string.camera_permission_needed)
+            finish()
+        }
+    }
+
+    private fun setupARCore() {
+        config = Config.createDefaultConfig()
+        session = Session(this)
+
+        if (!session.isSupported(config)) {
+            showToast(R.string.ar_device_not_supported)
             finish()
         }
     }
 }
 
-
 private fun Context.hasCameraPermission() =
         checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+
+private fun Context.showToast(@StringRes stringRes: Int) =
+    Toast.makeText(this, getString(stringRes), Toast.LENGTH_LONG).show()
