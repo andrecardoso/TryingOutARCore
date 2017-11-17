b736ebbf87429dfbed18e31f89dc3d16946e32c3
André Luiz Cardoso <andrecardoso@gmail.com>
2017-11-17 19:54:50 -0200

    Rendering Andy androids on the center of each detected plane

--- app/src/main/java/andrecardoso/tryingoutarcore/MainActivity.kt
+++ app/src/main/java/andrecardoso/tryingoutarcore/MainActivity.kt
@@ -1,6 +1,7 @@
 package andrecardoso.tryingoutarcore
 
 import andrecardoso.tryingoutarcore.rendering.BackgroundRenderer
+import andrecardoso.tryingoutarcore.rendering.ObjectRenderer
 import android.Manifest
 import android.app.Activity
 import android.content.Context
@@ -15,6 +16,7 @@ import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import com.google.ar.core.Config
+import com.google.ar.core.Plane
 import com.google.ar.core.Session
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.microedition.khronos.egl.EGLConfig
@@ -25,9 +27,16 @@ class MainActivity : AppCompatActivity(), GLSurfaceView.Renderer {
     private companion object {
         val TAG = "MainActivity"
         val CAMERA_PERMISSION_CODE = 0
+        val ARCORE_MATRIX_SIZE = 16
     }
 
     private val backgroundRenderer = BackgroundRenderer()
+    private val androidRenderer = ObjectRenderer()
+    private val androidShadowRenderer = ObjectRenderer()
+
+    private val viewMatrix = FloatArray(ARCORE_MATRIX_SIZE)
+    private val projectionMatrix = FloatArray(ARCORE_MATRIX_SIZE)
+    private val planeCenterMatrix = FloatArray(ARCORE_MATRIX_SIZE)
 
     lateinit var session: Session
     lateinit var config: Config
@@ -81,6 +90,31 @@ class MainActivity : AppCompatActivity(), GLSurfaceView.Renderer {
             // camera framerate.
             val frame = session.update()
             backgroundRenderer.draw(frame)
+
+            // Get projection matrix.
+            session.getProjectionMatrix(projectionMatrix, 0, 0.1f, 100.0f)
+
+            // Get camera matrix and draw.
+            frame.getViewMatrix(viewMatrix, 0)
+
+            // Compute lighting from average intensity of the image.
+            val lightIntensity = frame.lightEstimate.pixelIntensity
+
+            val scaleFactor = 1.0f
+            session.allPlanes.forEach { plane ->
+                if (plane.trackingState == Plane.TrackingState.TRACKING && plane.type == Plane.Type.HORIZONTAL_UPWARD_FACING) {
+                    // Get the current combined pose of an Anchor and Plane in world space. The Anchor
+                    // and Plane poses are updated during calls to session.update() as ARCore refines
+                    // its estimate of the world.
+                    plane.centerPose.toMatrix(planeCenterMatrix, 0)
+
+                    // Update and draw the model and its shadow.
+                    androidRenderer.updateModelMatrix(planeCenterMatrix, scaleFactor)
+                    androidShadowRenderer.updateModelMatrix(planeCenterMatrix, scaleFactor)
+                    androidRenderer.draw(viewMatrix, projectionMatrix, lightIntensity)
+                    androidShadowRenderer.draw(viewMatrix, projectionMatrix, lightIntensity)
+                }
+            }
         } catch (t: Throwable) {
             // Avoid crashing the application due to unhandled exceptions.
             Log.e(TAG, "Exception on the OpenGL thread", t)
@@ -100,6 +134,13 @@ class MainActivity : AppCompatActivity(), GLSurfaceView.Renderer {
         // Create the texture and pass it to ARCore session to be filled during update().
         backgroundRenderer.createOnGlThread(this)
         session.setCameraTextureName(backgroundRenderer.textureId)
+
+        androidRenderer.createOnGlThread(this, "andy.obj", "andy.png")
+        androidRenderer.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f)
+
+        androidShadowRenderer.createOnGlThread(this,"andy_shadow.obj", "andy_shadow.png")
+        androidShadowRenderer.setBlendMode(ObjectRenderer.BlendMode.Shadow)
+        androidShadowRenderer.setMaterialProperties(1.0f, 0.0f, 0.0f, 1.0f)
     }
 
     private fun setupSurfaceView() {
